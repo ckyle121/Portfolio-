9ddf5ab826f11bd0fe5fa0fe98d90f8d4ccb6c66
ckyle121 <carolinekyle21@gmail.com>
2022-04-28 15:14:49 -0500

    contact page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,10 +3,16 @@ import React from "react";
 function Card({ project }) {
     return(
         <div key={project.name} className="col-sm-12 col-md-6">
-        <a href={project.repo} target="_blank">
-        <h3>{project.name}</h3>
-        <img src={project.pic} alt={project.name}/>
-        </a>
+            <a href={project.repo} target="_blank">
+                <h3 className="text-center">{project.name}</h3>
+            </a>
+            <p>
+                {project.description}
+            </p>
+            <a href={project.repo} target="_blank">
+                <img src={project.pic} alt={project.name}/>
+            </a>
+            
         </div>
     );
 }
